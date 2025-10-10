0225fb6f1f3dde8837674ce86806661357fb064e
DevFahad <github.com/Fahad4345>
2025-10-10 17:01:19 +0500

    Changes to be committed: 	modified:   src/app/admin/dashboard/adminOrder/page.jsx

--- src/app/admin/dashboard/adminOrder/page.jsx
+++ src/app/admin/dashboard/adminOrder/page.jsx
@@ -222,13 +222,13 @@ export default function OrdersDashboard() {
                                                     ) : (
                                                         <>
                                                             <button
-                                                                onClick={() => handleDeclineOrder(order._id)}
+                                                                onClick={(e) => { handleDeclineOrder(order._id); e.preventDefault(); e.stopPropagation }}
                                                                 className='bg-[#DB4444] text-white px-[20px] py-[5px] rounded-[4px]'
                                                             >
                                                                 Decline
                                                             </button>
                                                             <button
-                                                                onClick={() => handleAcceptOrder(order._id)}
+                                                                onClick={(e) => { handleAcceptOrder(order._id); e.preventDefault(); e.stopPropagation }}
                                                                 className='bg-green-500 text-white px-[20px] py-[5px] rounded-[4px]'
                                                             >
                                                                 Accept
