cba19e045858342d01b042d18f9edfe44e047418
DevFahad <github.com/Fahad4345>
2025-10-14 16:51:29 +0500

    Changes to be committed: 	modified:   src/app/success/page.jsx

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect } from 'react'
 import Link from 'next/link'
+import Navbar from '../../Components/NavBar';
 
 export default function page() {
     useEffect(() => {
